b443392f01b2723b7cededc9d185da3766d2d676
GaziWani1 <gaziwani123@gmail.com>
2026-02-03 19:04:54 +0530

    pub/sub

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express from 'express';
-import { matchRouter } from './routes/matches.js';
 import http from 'http';
-import { attachWebSocketServer } from './ws/server.js';
-import { securityMiddleware } from './arcjet.js';
+import { matchRouter } from "./routes/matches.js";
+import { attachWebSocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./arcjet.js";
+import { commentaryRouter } from "./routes/commentary.js";
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || '0.0.0.0';
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 app.use(express.json());
-app.use(securityMiddleware())
 
-app.get('/', (_, res) => {
-    res.status(200).json({
-        msg: "App is running"
-    })
-})
+app.get('/', (req, res) => {
+    res.send('Hello from Express server!');
+});
+
+// app.use(securityMiddleware());
 
 app.use('/matches', matchRouter);
+app.use('/matches/:id/commentary', commentaryRouter);
 
-const { broadCastMatchCreated } = attachWebSocketServer(server);
-app.locals.broadCastMatchCreated = broadCastMatchCreated;
+const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
 
-server.listen(PORT, () => {
+server.listen(PORT, HOST, () => {
     const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
-    console.log(`SERVER IS RUNNING ON ${baseUrl}`);
-    console.log(`WEBSOCKET SERVER IS RUNNING ON ${baseUrl.replace('http', 'ws')}/ws`)
-})
+
+    console.log(`Server is running on ${baseUrl}`);
+    console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
+});
